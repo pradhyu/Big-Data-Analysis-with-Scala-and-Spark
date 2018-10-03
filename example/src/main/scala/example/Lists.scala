4d47adbe657912d7befd459f14c132574d2136c9
PK <pradhyu.shrestha@gmail.com>
2018-10-02 23:33:26 -0400

    initilal commit reset

--- example/src/main/scala/example/Lists.scala
+++ example/src/main/scala/example/Lists.scala
@@ -1,7 +1,5 @@
 package example
 
-import java.util.NoSuchElementException
-
 
 object Lists {
 
@@ -25,10 +23,7 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-    def sum(xs: List[Int]): Int = {
-      if (xs.isEmpty) return 0
-      xs.head + sum(xs.tail)
-    }
+    def sum(xs: List[Int]): Int = ???
   
   /**
    * This method returns the largest element in a list of integers. If the
@@ -43,16 +38,5 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-    def max(xs: List[Int]): Int = {
-      if (xs.isEmpty) throw new NoSuchElementException("empty list as parameter")
-      def maxOne(initialMax: Int, xs: List[Int]): Int = {
-        if (xs.isEmpty) return initialMax
-        if (initialMax >= xs.head) {
-          maxOne(initialMax, xs.tail)
-        } else {
-          maxOne(xs.head,xs.tail)
-        }
-      }
-      maxOne(xs.head,xs.tail)
-    }
-}
+    def max(xs: List[Int]): Int = ???
+  }
