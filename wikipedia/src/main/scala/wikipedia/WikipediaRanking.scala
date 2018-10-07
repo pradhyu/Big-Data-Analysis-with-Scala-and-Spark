5c61543f8f95a5758ab658a7eebcabd769047e9c
PK <pradhyu.shrestha@gmail.com>
2018-10-07 14:50:03 -0400

    squashed commit

--- wikipedia/src/main/scala/wikipedia/WikipediaRanking.scala
+++ wikipedia/src/main/scala/wikipedia/WikipediaRanking.scala
@@ -75,7 +75,9 @@ object WikipediaRanking {
    *   Note: this operation is long-running. It can potentially run for
    *   several seconds.
    */
-  def rankLangsUsingIndex(index: RDD[(String, Iterable[WikipediaArticle])]): List[(String, Int)] = ???
+  def rankLangsUsingIndex(index: RDD[(String, Iterable[WikipediaArticle])]): List[(String, Int)] = {
+    index.map(f => (f._1,f._2.size)).collect.toList.sortWith((a,b) => a._2 > b._2)
+  }
 
   /* (3) Use `reduceByKey` so that the computation of the index and the ranking are combined.
    *     Can you notice an improvement in performance compared to measuring *both* the computation of the index
@@ -84,7 +86,14 @@ object WikipediaRanking {
    *   Note: this operation is long-running. It can potentially run for
    *   several seconds.
    */
-  def rankLangsReduceByKey(langs: List[String], rdd: RDD[WikipediaArticle]): List[(String, Int)] = ???
+  def rankLangsReduceByKey(langs: List[String], rdd: RDD[WikipediaArticle]): List[(String, Int)] = {
+    rdd.flatMap( art =>
+      langs.filter(lang =>
+        art.mentionsLanguage(lang)).map(lang =>
+        (lang, 1))
+    ).reduceByKey(_ + _).collect.toList.sortWith((a,b) => a._2 > b._2)
+
+  }
 
   def main(args: Array[String]) {
 
